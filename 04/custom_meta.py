044dc9c059c4b16e93d04a7ae340d6b9133cb47d
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-04-01 07:50:08 +0300

    Add custom_meta.py file

---
+++ 04/custom_meta.py
@@ -0,0 +1,19 @@
+class CustomMeta(type):
+    def __new__(mcs, name, bases, dct):
+        new_dct = {}
+        for key, value in dct.items():
+            if not key.startswith("__"):
+                new_dct["custom_" + key] = value
+            else:
+                new_dct[key] = value
+
+        new_class = super().__new__(mcs, name, bases, new_dct)
+
+        def custom_setattr(self, name, value):
+            if not name.startswith("__") and not hasattr(self, name):
+                name = "custom_" + name
+
+            self.__dict__[name] = value
+
+        new_class.__setattr__ = custom_setattr
+        return new_class
