1007465f849142d15093494c146b25ff704d5cd4
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-04-03 23:56:16 +0300

    Update tests for evaluation_message.py

--- 01/evaluation_message_test.py
+++ 01/evaluation_message_test.py
@@ -10,55 +10,66 @@ class TestPredictMessageMood(unittest.TestCase):
     def test_predict_message_mood(self):
         with mock.patch.object(SomeModel, 'predict') as func:
             func.return_value = 0.1
-            result = predict_message_mood("This is a message", self.model)
+            result = predict_message_mood("This is a message 1", self.model)
             self.assertEqual(result, "неуд")
+            self.assertEqual([mock.call("This is a message 1")], func.mock_calls)
 
             func.return_value = 0.5
-            result = predict_message_mood("This is a message", self.model)
+            result = predict_message_mood("This is a message 2", self.model)
             self.assertEqual(result, "норм")
+            self.assertEqual(mock.call("This is a message 2"), func.mock_calls[1])
 
             func.return_value = 0.9
-            result = predict_message_mood("This is a message", self.model)
+            result = predict_message_mood("This is a message 3", self.model)
             self.assertEqual(result, "отл")
+            self.assertEqual(mock.call("This is a message 3"), func.mock_calls[2])
 
     def test_predict_message_mood_exact_threshold(self):
         with mock.patch.object(SomeModel, 'predict') as func:
             func.return_value = 0.3
-            result = predict_message_mood("This is a message", self.model)
+            result = predict_message_mood("This is a message 1", self.model)
             self.assertEqual(result, "норм")
+            self.assertEqual([mock.call("This is a message 1")], func.mock_calls)
 
             func.return_value = 0.8
-            result = predict_message_mood("This is a message", self.model)
+            result = predict_message_mood("This is a message 2", self.model)
             self.assertEqual(result, "норм")
+            self.assertEqual(mock.call("This is a message 2"), func.mock_calls[1])
 
     def test_predict_message_mood_custom_threshold(self):
         with mock.patch.object(SomeModel, 'predict') as func:
             func.return_value = 0.1
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.6, good_threshold=1.7)
+            result = predict_message_mood("This is a message 1", self.model, bad_threshold=0.6, good_threshold=1.7)
             self.assertEqual(result, "неуд")
+            self.assertEqual([mock.call("This is a message 1")], func.mock_calls)
 
             func.return_value = 1.0
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.6, good_threshold=1.7)
+            result = predict_message_mood("This is a message 2", self.model, bad_threshold=0.6, good_threshold=1.7)
             self.assertEqual(result, "норм")
+            self.assertEqual(mock.call("This is a message 2"), func.mock_calls[1])
 
             func.return_value = 2.9
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.6,
+            result = predict_message_mood("This is a message 3", self.model, bad_threshold=0.6,
                                           good_threshold=1.7)
             self.assertEqual(result, "отл")
+            self.assertEqual(mock.call("This is a message 3"), func.mock_calls[2])
 
     def test_predict_message_mood_bad_eq_good_threshold(self):
         with mock.patch.object(SomeModel, 'predict') as func:
             func.return_value = 0.5
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.5, good_threshold=0.5)
+            result = predict_message_mood("This is a message 1", self.model, bad_threshold=0.5, good_threshold=0.5)
             self.assertEqual(result, "норм")
+            self.assertEqual([mock.call("This is a message 1")], func.mock_calls)
 
             func.return_value = 0.4
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.5, good_threshold=0.5)
+            result = predict_message_mood("This is a message 2", self.model, bad_threshold=0.5, good_threshold=0.5)
             self.assertEqual(result, "неуд")
+            self.assertEqual(mock.call("This is a message 2"), func.mock_calls[1])
 
             func.return_value = 0.6
-            result = predict_message_mood("This is a message", self.model, bad_threshold=0.5, good_threshold=0.5)
+            result = predict_message_mood("This is a message 3", self.model, bad_threshold=0.5, good_threshold=0.5)
             self.assertEqual(result, "отл")
+            self.assertEqual(mock.call("This is a message 3"), func.mock_calls[2])
 
     def test_predict_message_mood_swap_thresholds(self):
         with mock.patch.object(SomeModel, 'predict') as func:
