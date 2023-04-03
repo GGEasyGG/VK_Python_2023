368f6c714d99314f121b6a50d2c8241dc7fc85e3
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-04-03 23:56:59 +0300

    Add support of file name

--- 01/file_generator.py
+++ 01/file_generator.py
@@ -1,19 +1,27 @@
-from typing import List, Iterator, TextIO
+from typing import List, Iterator, TextIO, Union
 from io import TextIOBase
 
 
-def search_lines(file: TextIO, words: List[str]) -> Iterator[str]:
+def search_lines(file: Union[TextIO, str], words: List[str]) -> Iterator[str]:
     if not isinstance(words, list):
         raise TypeError
     else:
         if not all(isinstance(elem, str) for elem in words):
             raise TypeError
 
-    if not isinstance(file, TextIOBase):
+    if not isinstance(file, TextIOBase) and not isinstance(file, str):
         raise TypeError
 
-    for line in file:
+    if isinstance(file, str):
+        f = open(file, 'r')
+    else:
+        f = file
+
+    for line in f:
         for word in words:
             if word.lower() in line.lower().split():
                 yield line.rstrip('\n')
                 break
+
+    if isinstance(file, str):
+        f.close()
