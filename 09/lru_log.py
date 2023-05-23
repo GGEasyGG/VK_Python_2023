d816ddc402b3f4f04794cfae217bd293d5d58868
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-05-23 21:09:08 +0300

    Update lru_log.py file

--- 09/lru_log.py
+++ 09/lru_log.py
@@ -3,9 +3,9 @@ import sys
 
 
 class LRUCache:
-    def __init__(self, limit=42):
+    def __init__(self, logger, filter_func, limit=42):
         self.filter_enabled = "-f" in sys.argv
-        self.logger = self._setup_logger()
+        self.logger = logger(self, filter_func)
 
         if not isinstance(limit, int):
             raise TypeError("The cache size must be of type int")
@@ -42,44 +42,45 @@ class LRUCache:
         self.cache[key] = elem
         self.logger.debug("Key '%s' moved to end", key)
 
-    def _setup_logger(self):
-        logger = logging.getLogger("LRUCache")
-        logger.setLevel(logging.DEBUG)
 
-        file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-        file_handler = logging.FileHandler("cache.log")
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(file_formatter)
+def setup_logger(obj, filter_func):
+    logger = logging.getLogger("LRUCache")
+    logger.setLevel(logging.DEBUG)
 
-        if self.filter_enabled:
-            file_handler.addFilter(self._apply_filter)
+    file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler = logging.FileHandler("cache.log")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(file_formatter)
 
-        logger.addHandler(file_handler)
+    if obj.filter_enabled:
+        file_handler.addFilter(filter_func)
 
-        if "-s" in sys.argv:
-            stdout_formatter = logging.Formatter("%(levelname)s: %(message)s")
-            stdout_handler = logging.StreamHandler(sys.stdout)
-            stdout_handler.setLevel(logging.DEBUG)
-            stdout_handler.setFormatter(stdout_formatter)
+    logger.addHandler(file_handler)
 
-            if self.filter_enabled:
-                stdout_handler.addFilter(self._apply_filter)
+    if "-s" in sys.argv:
+        stdout_formatter = logging.Formatter("%(levelname)s: %(message)s")
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)
+        stdout_handler.setFormatter(stdout_formatter)
 
-            logger.addHandler(stdout_handler)
+        if obj.filter_enabled:
+            stdout_handler.addFilter(filter_func)
 
-        return logger
+        logger.addHandler(stdout_handler)
 
-    def _apply_filter(self, record):
-        if self.filter_enabled:
-            message = record.getMessage()
-            words = message.split()
-            if len(words) % 2 == 0:
-                return False
-        return True
+    return logger
+
+
+def apply_filter(record):
+    message = record.getMessage()
+    words = message.split()
+    if len(words) % 2 == 0:
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    cache = LRUCache(limit=3)
+    cache = LRUCache(setup_logger, apply_filter, limit=3)
     cache.set("key1", "value1")
     cache.set("key2", "value2")
     cache.set("key3", "value3")
